da3fb818d6a562685ddfbaf3d257ab8c96c35b54
Nicolas Belisário <nick2007369@gmail.com>
2026-03-29 22:31:11 -0300

    Aula 6
    colisores com tilemap

--- scripts/player.gd
+++ scripts/player.gd
@@ -22,16 +22,15 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	if is_on_floor():
-		if direction > 0:
-			anim.flip_h = false
-			anim.play("walk")
-		elif direction < 0:
-			anim.flip_h = true
-			anim.play("walk")
-		else:
-			anim.play("idle")
+	if direction > 0:
+		anim.flip_h = false
+		anim.play("walk")
+	elif direction < 0:
+		anim.flip_h = true
+		anim.play("walk")
 	else:
+		anim.play("idle")
+	if not is_on_floor():
 		anim.play("jump")
 		
 	move_and_slide()
